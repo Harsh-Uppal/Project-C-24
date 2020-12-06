82e977e3f9b45a598ed50bd83383ec9b64f3b2c4
Harsh-Uppal <69905917+Harsh-Uppal@users.noreply.github.com>
2020-12-06 13:34:32 +0530

    Update ball.js

--- ball.js
+++ ball.js
@@ -1,7 +1,7 @@
 class ball{
     constructor(x,y,scale,options){
         this.scale = scale;
-        this.ballObject = Bodies.circle(x,y,scale*2,options);
+        this.ballObject = Bodies.circle(x,y,scale/2,options);
         World.add(world,this.ballObject);
     }
 
